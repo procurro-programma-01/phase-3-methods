a3688fe8667abf5f548cccc64b051a2d5fa43652
abraham saldivar <saldivara1010@gmail.com>
2021-08-09 23:31:49 -0500

    commit tree 604583c40e2acd7cb51bc18dc581ce1147696761

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
 # Your code here!
+def greet_programmer
+   puts "Hello, programmer!"
+end
+
+def greet(name = "Naureen")
+  puts "Hello, #{name}!"
+end
+greet
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+def add(x , y)
+  return x + y 
+end 
+
+def halve(number)
+  if number.class != Integer
+    return nil
+  end
+
+  number / 2 
+end
